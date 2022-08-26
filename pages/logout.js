832107e8c83fce833e4b55d1b1d2b302e2f30efe
Jorleidis Monsalvo <jp.monsalvo@uniandes.edu.co>
2022-08-26 15:00:48 -0500

    Fix vercel errors to deploy

--- pages/logout.js
+++ pages/logout.js
@@ -1,21 +1,20 @@
-import {useEffect} from "react";
-import { useRouter } from "next/dist/client/router";
+import React from "react";
 
-const logout = () => {
+const Logout = () => {
+  const { push } = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      push("/");
+    }, 3000);
+  }, []);
 
-    const {push} = useRouter()
-    useEffect(() => {
-        setTimeout(() => {
-            push('/')
-          }, 3000);
-    }, [])
-    
   return (
     <div className="register-form">
       <div className="logout-text">
-        <h4>Gracias por visitarnos!</h4></div>
+        <h4>Gracias por visitarnos!</h4>
+      </div>
     </div>
   );
 };
 
-export default logout;
+export default Logout;
