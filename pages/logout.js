835468196e39c940657cee9a7bcd3145fb979228
Jorleidis Monsalvo <jp.monsalvo@uniandes.edu.co>
2022-08-26 15:07:07 -0500

    fix import errors is logout

--- pages/logout.js
+++ pages/logout.js
@@ -1,5 +1,5 @@
-import React from "react";
-
+import {useEffect} from "react";
+import { useRouter } from "next/router";
 const Logout = () => {
   const { push } = useRouter();
   useEffect(() => {
